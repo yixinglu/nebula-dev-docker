ee379541fbae39261107793c9595423ada6f75c9
yixinglu <2520865+yixinglu@users.noreply.github.com>
2019-09-10 09:56:44 +0800

    Rename install path

--- install-deps.sh
+++ install-deps.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-DEVTOOLSET_PATH=/opt/rh/devtoolset-8/root
+INSTALL_PATH=/opt/rh/devtoolset-8/root/usr
 NEBULA_HOME=/home/nebula
 EXTRA_CXXFLAGS="-O2 -m64 -march=x86-64"
 EXTRA_PIC_CXXFLAGS="${EXTRA_CXXFLAGS} -fPIC -DPIC"
@@ -17,7 +17,7 @@ wget https://www.openssl.org/source/old/1.1.0/openssl-1.1.0h.tar.gz
 tar zxf openssl-1.1.0h.tar.gz
 pushd
 cd openssl-1.1.0h
-./config --prefix=${DEVTOOLSET_PATH}/usr --openssldir=${DEVTOOLSET_PATH}/usr/ssl no-shared threads ${EXTRA_CXXFLAGS} ${EXTRA_LDFLAGS}
+./config --prefix=${INSTALL_PATH} --openssldir=${INSTALL_PATH}/ssl no-shared threads ${EXTRA_CXXFLAGS} ${EXTRA_LDFLAGS}
 make && make install
 popd
 
@@ -27,7 +27,7 @@ tar zxf cmake-3.11.4.tar.gz
 pushd
 cd cmake-3.11.4
 . /etc/profile.d/devtoolset-8.sh
-CXXFLAGS=${EXTRA_PIC_CXXFLAGS} CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS="${EXTRA_LDFLAGS} -pthread" ./configure --prefix=${DEVTOOLSET_PATH}/usr
+CXXFLAGS=${EXTRA_PIC_CXXFLAGS} CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS="${EXTRA_LDFLAGS} -pthread" ./configure --prefix=${INSTALL_PATH}
 make && make install
 popd
 
@@ -37,7 +37,7 @@ tar xf bison-3.0.5.tar.gz
 pushd
 cd bison-3.0.5
 . /etc/profile.d/devtoolset-8.sh
-CXXFLAGS=${EXTRA_PIC_CXXFLAGS} CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS=${EXTRA_LDFLAGS} ./configure --prefix=${DEVTOOLSET_PATH}/usr --enable-shared=no --enable-static
+CXXFLAGS=${EXTRA_PIC_CXXFLAGS} CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS=${EXTRA_LDFLAGS} ./configure --prefix=${INSTALL_PATH} --enable-shared=no --enable-static
 make && make install
 popd
 
@@ -47,7 +47,7 @@ tar zxf flex-2.6.4.tar.gz
 pushd
 cd flex-2.6.4
 . /etc/profile.d/devtoolset-8.sh
-CXXFLAGS=${EXTRA_PIC_CXXFLAGS} CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS=${EXTRA_LDFLAGS} ./configure --prefix=${DEVTOOLSET_PATH}/usr --enable-shared=no
+CXXFLAGS=${EXTRA_PIC_CXXFLAGS} CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS=${EXTRA_LDFLAGS} ./configure --prefix=${INSTALL_PATH} --enable-shared=no
 make && make install
 popd
 
@@ -57,7 +57,7 @@ tar zxf boost_1_66_0.tar.gz
 pushd
 cd boost_1_66_0
 . /etc/profile.d/devtoolset-8.sh
-./bootstrap.sh --prefix=${DEVTOOLSET_PATH}/usr --without-icu --without-libraries=python
+./bootstrap.sh --prefix=${INSTALL_PATH} --without-icu --without-libraries=python
 ./b2 cxxflags=${EXTRA_CXXFLAGS} link=static runtime-link=static install
 ./b2 --clean-all
 popd
@@ -68,7 +68,7 @@ tar zxf gperf-3.1.tar.gz
 pushd
 cd gperf-3.1
 . /etc/profile.d/devtoolset-8.sh
-CXXFLAGS=${EXTRA_PIC_CXXFLAGS} CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS=${EXTRA_LDFLAGS} ./configure --prefix=${DEVTOOLSET_PATH}/usr --enable-shared=no
+CXXFLAGS=${EXTRA_PIC_CXXFLAGS} CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS=${EXTRA_LDFLAGS} ./configure --prefix=${INSTALL_PATH} --enable-shared=no
 make && make install
 popd
 
@@ -79,7 +79,7 @@ pushd
 cd krb5-krb5-1.16.3-final/src
 . /etc/profile.d/devtoolset-8.sh
 autoreconf
-CXXFLAGS=$EXTRA_PIC_CXXFLAGS CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS=${EXTRA_LDFLAGS} ./configure --prefix=${DEVTOOLSET_PATH}/usr --enable-static --disable-shared --disable-rpath --disable-aesni --disable-thread-support
+CXXFLAGS=$EXTRA_PIC_CXXFLAGS CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS=${EXTRA_LDFLAGS} ./configure --prefix=${INSTALL_PATH} --enable-static --disable-shared --disable-rpath --disable-aesni --disable-thread-support
 make all && make install
 popd
 
@@ -89,7 +89,7 @@ tar zxf libunwind-1.2.1.tar.gz
 pushd
 cd libunwind-1.2.1
 . /etc/profile.d/devtoolset-8.sh
-CXXFLAGS=${EXTRA_PIC_CXXFLAGS} CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS=${EXTRA_LDFLAGS} ./configure --prefix=${DEVTOOLSET_PATH}/usr --enable-shared=no
+CXXFLAGS=${EXTRA_PIC_CXXFLAGS} CFLAGS=$CXXFLAGS CPPFLAGS=$CXXFLAGS LDFLAGS=${EXTRA_LDFLAGS} ./configure --prefix=${INSTALL_PATH} --enable-shared=no
 make && make install
 popd
 
